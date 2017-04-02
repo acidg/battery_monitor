007c1967a8bf16142881ed285bdaf440dcb05faa
Benedikt Schlagberger <schlagbe@in.tum.de>
2017-04-02 15:43:56 +0200

    adding content to last row

--- BatteryMonitor.ino
+++ BatteryMonitor.ino
@@ -11,12 +11,26 @@
 
 #define OLED_RESET 4
 #define BATTERY_FULL_RAW_VALUE 1023
+#define BATTERY_EMPTY_RAW_VALUE 856
+#define VCC_INPUT 5.0
+#define FACTOR_VOLTAGE_DIVIDER 0.014662757
+#define FACTOR_CURRENT_SENSOR VCC_INPUT / 1024 / 0.066 // ACS712 has 0.066V/A sensitivity
+
+/* Rows, heights and widths */
+#define FIRST_ROW_HEIGHT 8
+#define MIDDLE_ROW_X_START 8
+#define END_ROW_X_START 57
+#define MIDDLE_ROW_HEIGHT END_ROW_X_START - MIDDLE_ROW_X_START
+#define END_ROW_HEIGHT 8
+#define DISPLAY_X_MIDDLE SSD1306_LCDWIDTH / 2
 
 Adafruit_SSD1306 display(OLED_RESET);
 
 uint16_t raw_voltage_value = 0;
 uint16_t raw_current_value = 0;
 uint8_t percentage = 0;
+double voltage = 0;
+double current = 0;
 
 void setup() {
     Serial.begin(9600);
@@ -29,19 +43,35 @@ void setup() {
 }
 
 void loop() {
-    raw_voltage_value = analogRead(A0);
-    raw_current_value = analogRead(A3);
+	raw_voltage_value = analogRead(A0);
+	raw_current_value = analogRead(A3);
+	Serial.print("A0: ");
+	Serial.print(raw_voltage_value);
+	Serial.print(", A3: ");
+	Serial.print(raw_current_value);
 
-    percentage = 100 * raw_voltage_value/BATTERY_FULL_RAW_VALUE;
+	percentage = 100 * raw_voltage_value / BATTERY_FULL_RAW_VALUE;
 
-    update_percentage();
-    update_bar();
-//    update_details();
+	voltage = raw_voltage_value * FACTOR_VOLTAGE_DIVIDER;
+	current = ((int16_t) raw_current_value - 512) * FACTOR_CURRENT_SENSOR;
+
+	Serial.print(", V: ");
+	Serial.print(voltage);
+	Serial.print(", ");
+	Serial.print(percentage);
+	Serial.print("%, Factor: ");
+	Serial.print(FACTOR_CURRENT_SENSOR, 10);
+	Serial.print(", A: ");
+	Serial.println(current);
+
+	update_percentage();
+	update_bar();
+	update_details();
 }
 
 void update_percentage() {
-    display.fillRect(0, 8, display.width(), display.height() - 16, BLACK);
-    display.fillRect(0, 8, display.width() * percentage/100, display.height() - 16, WHITE);
+    display.fillRect(0, MIDDLE_ROW_X_START, display.width(), MIDDLE_ROW_HEIGHT, BLACK);
+    display.fillRect(0, MIDDLE_ROW_X_START, display.width() * percentage/100, MIDDLE_ROW_HEIGHT, WHITE);
     display.setTextColor(INVERSE);
     display.setTextWrap(false);
     display.setTextSize(1);
@@ -65,32 +95,36 @@ void update_percentage() {
 }
 
 void update_details() {
+    display.fillRect(0, END_ROW_X_START, display.width(), END_ROW_HEIGHT, BLACK);
     display.setTextColor(WHITE);
     display.setTextWrap(false);
     display.setTextSize(1);
     display.setFont();
+
     display.setCursor(0, 57);
-    display.print("RAW: ");
-    display.print(raw_voltage_value);
+    display.print(voltage, 1);
+    display.print("V ");
+    display.print(current, 1);
+    display.print("A ");
+
     display.display();
 }
 
 void update_bar() {
 	int8_t value = (raw_current_value - 512) / 8;
-	uint8_t middle = display.width() / 2;
 
 	// reset display area
-    display.fillRect(0, 0, display.width(), 7, BLACK);
-    display.drawLine(display.width() / 2, 0, middle, 7, WHITE);
+    display.fillRect(0, 0, display.width(), FIRST_ROW_HEIGHT - 1, BLACK);
+    display.drawLine(display.width() / 2, 0, DISPLAY_X_MIDDLE, FIRST_ROW_HEIGHT - 1, WHITE);
 
 	if (value != 0) {
-		uint8_t cursor_x = middle + value;
+		uint8_t cursor_x = DISPLAY_X_MIDDLE + value;
 		if (value > 0) {
-			display.fillRect(middle, 0, value, 3, WHITE);
-			display.fillTriangle(cursor_x, 0, cursor_x, 5, cursor_x + 6, 0, WHITE);
+			display.fillRect(DISPLAY_X_MIDDLE, 0, value, 3, WHITE);
+			display.fillTriangle(cursor_x, 0, cursor_x, FIRST_ROW_HEIGHT - 3, cursor_x + FIRST_ROW_HEIGHT - 3, 0, WHITE);
 		} else {
 			display.fillRect(cursor_x, 0, abs(value), 3, WHITE);
-			display.fillTriangle(cursor_x, 0, cursor_x, 5, cursor_x - 6, 0, WHITE);
+			display.fillTriangle(cursor_x, 0, cursor_x, FIRST_ROW_HEIGHT - 3, cursor_x - FIRST_ROW_HEIGHT + 3, 0, WHITE);
 		}
     }
 	display.display();
